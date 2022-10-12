fd832fdced564c81fa044b4303720fc3e07f44ce
tezaswi-raj <tezaswi777@gmail.com>
2022-10-12 20:08:01 +0530

    production env set

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientId: "1068687247599-9o182hc61jaclgad691uumf01erdajj7.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: "mongodb+srv://tezaswi_raj:8MvLMILORsp4i8Yc@cluster0.qzphpvu.mongodb.net/emailydb?retryWrites=true&w=majority",
+  cookieKey: "iencianijfonwoihnfonsihcnoijoirhgoincvaoiihoiajhioencijeriiuvirinskdnfos"
+}
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users')
 
+passport.serializeUser(( user, done ) => {
+  console.log("serialized user", user)
+  done(null, user.id)   // for each instance of user the mongo assigns each record an id and that id we are fetching as user.id
+} )
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    console.log("deserialized user", user)
+    done(null, user);
+  })  // this will find the user associated with the record with record id 'id'
+})
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientId,
   clientSecret: keys.googleClientSecret,
@@ -12,12 +24,19 @@ passport.use(new GoogleStrategy({
 }, (accessTokens, refreshToken, profile, done) => {
     User.findOne( { googleId: profile.id } ).then((existingUser) => {
         if(existingUser) {
+          console.log(existingUser,"existinguser")
           //we already have a record with a given profile id
-
+          done(null, existingUser)
+          // to signal passport that the authentication flow is completed and the process and proceed further we have to call done() function.
         }
         else {
           // we don't have a record with this id, make a new record
-          new User( { googleId: profile.id } ).save()
+          new User( { googleId: profile.id } )
+          .save()
+          .then((user) => {
+            console.log(user, "newuser")
+            done(null, user); // done accepts two arguments, the first argument is to report if there has been an error and second argument will tell the successfully authenticated user
+          })
         }
       })
  }
